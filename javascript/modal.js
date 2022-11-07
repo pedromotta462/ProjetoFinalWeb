d049226dd8e92ff4b622d664282cfae7fa35b7d6
Igor Pereira <igor.pereira@avenuecode.com>
2022-11-07 10:48:44 -0300

    .

--- javascript/modal.js
+++ javascript/modal.js
@@ -1,11 +1,7 @@
-
-
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
 }
-// Get the modal
 var modal = document.getElementById('modalid');
 
